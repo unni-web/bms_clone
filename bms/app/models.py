68f586482f94960ad538bcdcea9ad9b95d34aa14
unni-web <unnikrish1011@gmail.com>
2025-11-03 08:28:59 +0530

    90%

--- bms/app/models.py
+++ bms/app/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
+class Poster(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    doc=models.FileField(upload_to='uploads/')
+    
+    def __str__(self):
+        return str(self.doc)
--- bms/bms/urls.py
+++ bms/bms/urls.py
@@ -17,6 +17,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from app import views
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -27,3 +30,6 @@ urlpatterns = [
     path('logout/',views.logoutuser),
     path('book/',views.book)
 ]
+
+if settings.DEBUG:
+    urlpatterns+=static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
